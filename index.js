85dbeee2bd5b46d2bcc4d19b3634af484845d027
Felipe Vaz <felipevaz@live.com>
2019-08-18 17:04:25 -0300

    feature: adicionado hook para atualizaçao de repositorios

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ app.listen(3000)
 app.get('/', function (req, res) {
     res.send('Hello World!')
 })
+
+app.post('/hook/alpha', function (req, res) {
+    
+    var sender = req.body.sender;
+    var branch = req.body.ref;
+
+    console.log( req.body )
+
+    // if(branch.indexOf('master') > -1 && sender.login === 'fvaz1906'){
+    //     childProcess.exec('cd /var/www && ./deploy.sh', function(err, stdout, stderr){
+    //         if (err) {
+    //             console.error(err);
+    //             return res.send(500);
+    //         }
+    //         res.send(200);
+    //     });
+    // }
+
+})
